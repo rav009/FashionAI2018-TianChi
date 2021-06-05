13b4f0afdfbae9140c3660492e6634f34192024a
python wei <rav009_student@sina.com>
2021-06-06 01:11:32 +0800

    Add files via upload

--- my_train.bat
+++ my_train.bat
@@ -7,9 +7,14 @@ set batch_size=10
 set MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 
 set task_file_1=my_train_task_design_windows.py
-set task_file_2=train_task_length.py
+set task_file_2=my_train_task_length_windows.py
 
-python3 %task_file_1% --task collar_design_labels --model %model% --batch-size %batch_size% --num-gpus %num_gpus% -j %num_workers% --epochs %epoch% --lr-steps 7,12,15
+::python3 %task_file_1% --task collar_design_labels --model %model% --batch-size %batch_size% --num-gpus %num_gpus% -j %num_workers% --epochs %epoch% --lr-steps 7,12,15
+::python3 %task_file_1% --task neckline_design_labels --model %model% --batch-size %batch_size% --num-gpus %num_gpus% -j %num_workers% --epochs %epoch% --lr-steps 7,12,15
+::python3 %task_file_1% --task neck_design_labels --model %model% --batch-size %batch_size% --num-gpus %num_gpus% -j %num_workers% --epochs %epoch% --lr-steps 7,12,15
+::python3 %task_file_1% --task lapel_design_labels --model %model% --batch-size %batch_size% --num-gpus %num_gpus% -j %num_workers% --epochs %epoch% --lr-steps 7,12,15
+
+python3 %task_file_2% --task skirt_length_labels --model %model% --batch-size %batch_size% --num-gpus %num_gpus% -j %num_workers% --epochs %epoch% --lr-steps 7,12,15
 
 pause
 
